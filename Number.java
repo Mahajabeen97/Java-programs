3b50035905e1fdec52ba5811dfafe230de2e7d3c
Mahajabeen97 <35861281+Mahajabeen97@users.noreply.github.com>
2018-09-25 09:52:52 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,30 @@
+package javaapplication;
+ import java.util.Scanner;
+
+ 
+public class NumberCheck{
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is Zero ");
+        }
+    }
+}
+
+
