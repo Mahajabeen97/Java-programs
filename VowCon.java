54014654aea5b4a996652cfac72e60102c09cc8c
Mahajabeen97 <35861281+Mahajabeen97@users.noreply.github.com>
2018-12-19 15:00:19 +0530

    Update VowCon.java

--- VowCon.java
+++ VowCon.java
@@ -12,7 +12,12 @@ int alphabet;
  if(alphabet='a'||alphabet='e'||alphabet='i'||alphabet='o'|| alphabet='u')
 
   System.out.println("Vowel");
-  else
-   System.out.println("Consonant");
+ if(alphabet='b'||alphabet='c'||alphabet='d'||alphabet='f'||alphabet='g'||alphabet='h'||alphabet='j'||alphabet='k'||alphabet='l'||alphabet='m'||
+  alphabet='n'||alphabet='p'||alphabet='q'||alphabet='r'||alphabet='s'||alphabet='t'||alphabet='v'||alphabet='w'||alphabet='x'||alphabet='y'||alphabet='z')
+    System.out.println("Consonant");
+    else
+      System.out.println("Invalid");
+      
+      
  }
   }
